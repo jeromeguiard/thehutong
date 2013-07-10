1c7934881fd0c49705a9c77bac6a151719337a4b
root <root@debian>
2013-07-10 21:29:59 +0800

    fix install script

---
+++ sql_ini.sql
@@ -0,0 +1,3 @@
+create database thehutong CHARACTER SET="UTF8";
+create user thehutong identified by 'thehutong';
+grant all privileges on thehutong.* to 'thehutong'@'localhost' identified by "thehutong";
