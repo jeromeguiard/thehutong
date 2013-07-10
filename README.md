d666614cbd2b136eeb435e45938863300b7c8b87
Jerome <jerome.guiard@gmail.com>
2013-07-10 10:33:36 +0200

    First commit with part of the installation environnement
    	modified:   README.md
    	new file:   install_django.sh
    	new file:   manage.py
    	new file:   thehutong/__init__.py
    	new file:   thehutong/settings.py
    	new file:   thehutong/urls.py
    	new file:   thehutong/wsgi.py

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 thehutong
 =========
+
+Runs with django and mysql.
+
+Installation
+------------
+To install the environnment on a debian server:
+    $ chmod +x install_django.sh
+    $ ./install_django.sh
+
+
---
+++ install_django.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update
+apt-get install python python-dev python-setuptools -y
+apt-get install curl -y
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py && python get-pip.py
+pip install virtualenv
+mkdir ~/.virtualenv
+virtualenv ~/.virtualenv/thehutong 
+apt-get install mysql-server
