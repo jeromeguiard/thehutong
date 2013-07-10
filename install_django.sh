6f273c4480794b24ac309ed62af5e304fdab7a7a
Jerome <jerome.guiard@gmail.com>
2013-07-10 14:04:45 +0200

    Configuration file for django installation

--- install_django.sh
+++ install_django.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+#Update the database of the packet manager
 apt-get update
+
+#install python with modules for dev 
 apt-get install python python-dev python-setuptools -y
+
+#install pip
 apt-get install curl -y
 curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py && python get-pip.py
+
+#Create a virtual env
 pip install virtualenv
 mkdir ~/.virtualenv
 virtualenv ~/.virtualenv/thehutong 
-apt-get install mysql-server
 
-pip install 
+#Install the database
+sudo debconf-set-selections <<< 'mysql-server-5.5.31 mysql-server/root_password password P0ww0w'
+sudo debconf-set-selections <<< 'mysql-server-5.5.31 mysql-server/root_password_again password P0ww0w'
+
+apt-get install mysql-server libmysqlclient-dev mysql-python
+
+mysql -pP0ww0w < create database thehutong CHARACTER SET="UTF8";   
+mysql -pP0ww0w < create user thehutong identified by 'thehutong'; 
+mysql -pP0ww0w < grant all privileges on thehutong.* to 'thehutong'@'localhost' identified by "thehutong";
+
+#Install pip requirments
+pip install -e requirements.txt 
+
--- thehutong/settings.py
+++ thehutong/settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'thehutong',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'thehutong',
-        'PASSWORD': 'thehutong',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'OPTIONS' : {
+            'read_default_file': 'my.conf',
+        },
+    #    'NAME': 'thehutong',                      # Or path to database file if using sqlite3.
+    #    'USER': 'thehutong',
+    #    'PASSWORD': 'thehutong',
     }
 }
 
