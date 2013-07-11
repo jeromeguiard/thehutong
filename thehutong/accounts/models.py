a52166a10d368c6a32ecad2b95e54944eceed125
Jerome <jerome.guiard@gmail.com>
2013-07-11 05:40:37 +0200

    first models creation for accounts , fix of hunt models

--- thehutong/accounts/models.py
+++ thehutong/accounts/models.py
@@ -1,18 +1,60 @@
 from django.db import models
 from django.contrib.auth.models import User
+from hunt.models import Hunt, Challenge
 
 class Team(models.Model):
     """
     A team that is going to perfom hunts
     """
+    name = models.textField(_(u"Team name"),
+                           max_length=200)
+    user = models.OneToOneField(User,
+                                unique=True,
+                                verbose_name=_(u'user'))
+    created = models.DateTimeField(auto_now_add=True) 
+
+class ChallengeTeamHunt(models.Model):
+    """
+    Individual challenge performed by a team durin a hunt
+    """
+    COMPLETED = 1
+    IN_PROGRESS = 2
+    NOT_STARTED = 0
+
+    LOCKED = 0
+    UNLOCKED =1 
+
+    COMPLETED_STATUS=(
+        (COMPLETED, _(u'Completed')),
+        (IN_PROGRESS, _(u'In progress')),
+        (NOT_STARTED, _(u'Not started')),
+    )
+
+    LOCKED_STATUS=(
+        (LOCKED , _(u'Challenge locked')),
+        (UNLOCKED , _(u'Challenge unlock'))
+    )
+
+    challenge = models.ForeignKey(Challenge)
+    team = models.ForeignKey(Team)
+    points = models.IntegerField(help_text = _(u"Point earned while the challenge has been completed"),
+                                default=0)
+    status = models.IntegerField(_(u'Completed status for the challenge'),
+                                 choices=COMPLETED_STATUS,
+                                 default=NOT_STARTED)
+    comment = models.CharField(_(u'Comment about the challenge'),
+                              max_length = 140)
+    lock = models.IntegerField(_(u'Challenge lock status'),
+                              choices=LOCKED_STATUS,
+                              default=LOCKED)
 
 class TeamHunt(models.Model):
     """
     When a team start a hunt it will generate this object to follow it \
     individually
     """
-
-class ChallengeTeamHunt(models.Model):
-    """
-    Individual challenge performed by a team durin a hunt
-    """
+    team = models.ForeignKey(Team)
+    hunt = models.ForeignKey(Hunt)
+    created = models.DateTimeField(auto_now_add=True)
+    challenge = models.ManyToManyField(Challenge)
+    challengesSuccesed = models.ManyToManyField(Challenge)
--- thehutong/hunt/models.py
+++ thehutong/hunt/models.py
@@ -3,34 +3,48 @@ from django.contrib.translation import ugettext_lazy as _
 
 class PointOfInterest(models.Model):
     """
-    Point of interest represent a place where a user hs to be to complete\    a challenge
+    Point of interest represent a place where a user hs to be to complete\
+    a challenge
     """
-    title = models.CharField(_('Title'), max_length=255)
+    title = models.CharField(_(u'Title'),
+                             max_length=255)
     point = models.PointField(help_text="Represented as (longitude, latitude)")
     created = models.DateTimeField(auto_now_add=True)
-    radius = models.IntegerField(_('Radius in metter'), default=10)
-    description = models.TextField(_('Description of the POI'), blank=True)
-    photo = ImageField(upload_to='poi', null=True, blank=True)
+    radius = models.IntegerField(_(u'Radius in metter'),
+                                 default=10)
+    description = models.TextField(_(u'Description of the POI'),
+                                   blank=True)
+    photo = ImageField(upload_to='poi',
+                       null=True,
+                       blank=True)
 
 class Challenge(models.Model):
     """
     A challenge to complete to a special point of interest during a hunt
     """
-    title = models.CharField(_('title'), max_length=255)
-    poi = models.ForeignKey(PointOfInterest, null=False)
-    numberOfPoints = models.IntegerField(_('Number of points the challenge represents'))
-    question = models.TextField(_('Specific question for the challenge'))
-    answer = models.CharField(_("Question answer"), max_length=255)
+    title = models.CharField(_(u'title'),
+                             max_length=255)
+    poi = models.ForeignKey(PointOfInterest,
+                            null=False)
+    numberOfPoints = models.IntegerField(_(u'Number of points the challenge represents'))
+    question = models.TextField(_(u'Specific question for the challenge'))
+    answer = models.CharField(_("Question answer"),
+                              max_length=255)
 
 class Hunt(models.Model):
     """
     A hunt is a list of challenge that team need to perform in order to\
     get as much point as possible.
     """
-    title = modelsCharField(_('title'), max_length=255)
-    startingPOI =  models.ForeignKey(PointOfInterest, null=False)
-    endingPOI =  models.ForeignKey(PointOfInterest, null=False)
-    challenges = 
-    duration = 
-    unlockingPass = models.TextField(_('Specific password for the hunt'))
-    latePenalty = models.IntegerField(_('Penalty applied to the final score in case of delay', default=0) 
+    title = models.CharField(_(u'title'),
+                            max_length=255)
+    startingPOI =  models.ForeignKey(PointOfInterest, 
+                                     null=False)
+    endingPOI =  models.ForeignKey(PointOfInterest, 
+                                   null=False)
+    challenges = models.ManyToMany(Challenge) 
+    duration = models.IntegerField(help_text=_(u"Durationof the hunt in minutes"),
+                                   default=180) 
+    unlockingPass = models.TextField(_(u'Specific password for the hunt'))
+    latePenalty = models.IntegerField(_(u'Penalty applied to the final score in case of delay',
+                                      default=0) 
