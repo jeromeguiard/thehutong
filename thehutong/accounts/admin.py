a944897050fde8b9c0a5e383ef79833a9aec29e4
Jerome <jerome.guiard@gmail.com>
2013-07-11 11:21:28 +0200

    add account admin

---
+++ thehutong/accounts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Team, ChallengeTeamHunt, TeamHunt  
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Team, TeamAdmin)
+
+class ChallengeTeamHuntAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ChallengeTeamHunt, ChallengeTeamHuntAdmin)
+
+class TeamHuntAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(TeamHunt, TeamHuntAdmin)
