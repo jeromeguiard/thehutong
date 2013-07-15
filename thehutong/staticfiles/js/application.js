b1f7bf9ebe4d5b0d8ec107a9bf6dd7c156a79ec6
Jerome <jerome.guiard@gmail.com>
2013-07-15 08:53:07 +0200

    add templates file

--- thehutong/staticfiles/js/application.js
+++ thehutong/staticfiles/js/application.js
@@ -40,16 +40,44 @@ function getHunts(){
 
 function displayHunts(objects){
     console.log(objects);
-    var container =document.getElementById("container");
+    var container = document.getElementById("container");
     container.innerHTML = "";
     objects.objects.forEach(function(element){
+        sessionStorage.setItem("hunt_"+element.id, JSON.stringify(element)); 
         var objectDiv = document.createElement("div");
         objectDiv.setAttribute("class","row-fluid hero-unit");
-        objectDiv.innerHTML = "<div class=\"span4\"><div>"+element.title +"</div><div>Hunt from "+element.startingPOI.title+" to "+ element.endingPOI.title+"</div><a class=\"btn\">View hunt</a></div>";
+        objectDiv.innerHTML = "<div class=\"span4\"><div>"+element.title +"</div><div>Hunt from "+element.startingPOI.title+" to "+ element.endingPOI.title+"</div><span class=\"btn\" onclick=\"viewHuntDetails("+ element.id+");\">View hunt</span></div>";
         container.appendChild(objectDiv);
     }
     );
 }
+
+function viewHuntDetails(huntId){
+    huntInfo = JSON.parse(sessionStorage.getItem("hunt_"+huntId));
+    var container = document.getElementById("container");
+    container.innerHTML = "";
+    objectDiv = document.createElement("div"); 
+    objectDiv.innerHTML = "<div class=\"hero-unit\" <div>"+huntInfo.title+"</div> <div>Starting from : "+ huntInfo.startingPOI.title+"</div><div>Ending at  : "+ huntInfo.endingPOI.title+"</div><div>The hunt contains "+ huntInfo.challenges.length + " challenges. When you start it you will have "+ huntInfo.duration+" minutes to achieve all challenges.</div><span class=\"btn\" onclick=\"takePartInHunt("+ huntId+");\">Take part in hunt</span></div>";
+    container.appendChild(objectDiv);
+}
+
+function takePartInHunt(huntId, firstTry ){
+    if(typeof(firstTry)==='undefined') firstTry = true;
+    var password;
+    if (firstTry )
+        password = window.prompt("Password for the hunt:");
+    else
+        password = window.prompt("Wrong pass try again:");
+        
+    var huntInfo = JSON.parse(sessionStorage.getItem("hunt_"+huntId));
+    debugger;
+    if (password == huntInfo.unlockingPass){
+        alert("ok");
+    }else{
+       takePartInHunt(huntId, false); 
+    }
+}
+
 try{
     'localStorage' in window && window['localStorage'] !== null;
     generateLogin();
