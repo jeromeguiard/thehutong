3019c2bd261990b598ca36c4745bba2a55b9ea71
jerome <jerome.guiard@gmail.com>
2013-07-16 14:20:16 +0800

    update javascript

--- thehutong/staticfiles/js/application.js
+++ thehutong/staticfiles/js/application.js
@@ -1,3 +1,7 @@
+/*
+ * function used in order to generate the login form used to access the application
+ *
+ * */
 function generateLogin(){
     var container = document.getElementById("container");
     var containerLoginContent = document.createElement("div");
@@ -6,6 +10,11 @@ function generateLogin(){
     containerLoginContent.innerHTML= '<h1>Login</h1><input type="text" value="Team name" id="team"><br/></input><input type="password" id="password"></input><br/><input type="button" class="btn" value="login" onclick="login();"></input>';
     container.appendChild(containerLoginContent); 
 }
+/*
+ *Login process
+ *
+ * */
+
 function login(){
    var team = document.getElementById("team").value;
    var password = document.getElementById("password").value;
@@ -23,8 +32,14 @@ function login(){
                            getHunts();
                        }
                       };
+   
    request.send();
 }
+
+/*
+ * retrieve hunt informations
+ *
+ */
 
 function getHunts(){
     var request = new XMLHttpRequest();
@@ -39,6 +54,11 @@ function getHunts(){
    request.send();
 }
 
+/*
+ *Display hunts on the webpage and store informations in sessionstorage for fster purpose
+ *
+ */
+
 function displayHunts(objects){
     console.log(objects);
     var container = document.getElementById("container");
@@ -52,6 +72,10 @@ function displayHunts(objects){
     }
     );
 }
+/*
+ * Show from the sessionStorage informations about the hunt
+ *
+ */
 
 function viewHuntDetails(huntId){
     huntInfo = JSON.parse(sessionStorage.getItem("hunt_"+huntId));
@@ -61,6 +85,12 @@ function viewHuntDetails(huntId){
     objectDiv.innerHTML = "<div class=\"hero-unit\" <div>"+huntInfo.title+"</div> <div>Starting from : "+ huntInfo.startingPOI.title+"</div><div>Ending at  : "+ huntInfo.endingPOI.title+"</div><div>The hunt contains "+ huntInfo.challenges.length + " challenges. When you start it you will have "+ huntInfo.duration+" minutes to achieve all challenges.</div><span class=\"btn\" onclick=\"takePartInHunt("+ huntId+");\">Take part in hunt</span></div>";
     container.appendChild(objectDiv);
 }
+
+
+/*
+ * Validate the user password and let him take part in hunt
+ *
+ */
 
 function takePartInHunt(huntId, firstTry ){
     if(typeof(firstTry)==='undefined') firstTry = true;
@@ -84,12 +114,29 @@ function takePartInHunt(huntId, firstTry ){
                                              ":"+
                                              sessionStorage.getItem("key"));
         request.setRequestHeader("Content-Type","application/json");
-        request.onloadend =function(){ console.log(request.response);};
+        request.onloadend =function(){ 
+                             displayTeamHuntAndChallenge(request.getResponseHeader("Location"));};
         request.send(jsonTosend);
     }else{
        takePartInHunt(huntId, false); 
     }
 }
+
+/*
+ * Display challenges and store informaitons in session storage
+ *
+ */
+
+function displayTeamHuntAndChallenge(headers){
+    console.log(headers);
+}
+
+
+/*
+ *
+ *First execution with the test if local/session storage is on
+ *
+ */
 
 try{
     'localStorage' in window && window['localStorage'] !== null;
